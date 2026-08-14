ab591ba0722b4620e8feed973735a290f68955cc
maja <61113052+marijana-fi@users.noreply.github.com>
2026-08-14 16:37:14 +0200

    CORE-2797: update logo on utilities

--- components/JamLogo64x64.tsx
+++ components/JamLogo64x64.tsx
@@ -1,13 +1,20 @@
 export const JamLogo64x64 = () => {
-  const logoUrl = "https://storage.googleapis.com/jam-assets/jam-logo.webp";
-
   return (
-    <img
-      src={logoUrl}
-      className="rounded-xl overflow-hidden ring-1 ring-gray-400/10 shadow-md"
-      alt="Jam Logo"
-      width={64}
-      height={64}
-    />
+    <div className="w-16 h-16 p-2 rounded-xl overflow-hidden ring-1 ring-gray-400/10 shadow-md">
+      <img
+        src="/icons/jam-logo-mark-full-color-for-light-background-128x128.png"
+        className="block dark:hidden w-full h-full"
+        alt="Jam Logo"
+        width={64}
+        height={64}
+      />
+      <img
+        src="/icons/jam-logo-mark-full-color-for-dark-background-128x128.png"
+        className="hidden dark:block w-full h-full"
+        alt="Jam Logo"
+        width={64}
+        height={64}
+      />
+    </div>
   );
 };
