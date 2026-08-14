afb45b7e830b0380d0bfe70642b7c6c9bebb3ee5
Petar Cirkovic <pekiseven@gmail.com>
2026-08-14 16:58:23 +0200

    chore: update logo

--- components/JamLogo64x64.tsx
+++ components/JamLogo64x64.tsx
@@ -2,14 +2,14 @@ export const JamLogo64x64 = () => {
   return (
     <div className="w-16 h-16 p-2 rounded-xl overflow-hidden ring-1 ring-gray-400/10 shadow-md">
       <img
-        src="/icons/jam-logo-mark-full-color-for-light-background-128x128.png"
+        src="https://storage.googleapis.com/jam-assets/icons/jam.png"
         className="block dark:hidden w-full h-full"
         alt="Jam Logo"
         width={64}
         height={64}
       />
       <img
-        src="/icons/jam-logo-mark-full-color-for-dark-background-128x128.png"
+        src="https://storage.googleapis.com/jam-assets/icons/jam-dark.png"
         className="hidden dark:block w-full h-full"
         alt="Jam Logo"
         width={64}
